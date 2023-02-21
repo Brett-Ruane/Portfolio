271526609a2a1c420d6bd19dc37a3382cf80c52d
Brett-Ruane <bgr28288@uga.edu>
2023-02-21 13:47:06 -0500

    fixed contact and added textstack

---
+++ src/components/TextStack.js
@@ -0,0 +1,19 @@
+import './TextStack.css';
+
+function TextStack() {
+    return (
+      <div className="text-stack">
+        <h1>
+            TextStack1
+        </h1>
+        <h2>
+            TextStack2
+        </h2>
+        <h3>
+            TextStack3
+        </h3>
+      </div>
+    );
+  }
+
+export default TextStack;
