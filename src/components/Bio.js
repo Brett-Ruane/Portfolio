3a21bf57d277899334127e317fadee6f69f37aaf
Brett-Ruane <bgr28288@uga.edu>
2023-02-26 06:59:58 -0500

    adding scrolling text

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,15 +1,50 @@
 import "./Bio.css";
 
 function Bio() {
+  // const viewportWidth = document.getElementById("viewport-width");
+  // const viewportHeight = document.getElementById("viewport-height");
+  // viewportWidth.innerHTML = document.documentElement.clientWidth;
+  // viewportHeight.innerHTML = document.documentElement.clientHeight;
+
+  // const topEdge = document.getElementById("top");
+  // const bottomEdge = document.getElementById("bottom");
+
+  const bioDesc = document.querySelector(".desc");
+  document.addEventListener("scroll", function () {
+    const clientHeight = document.documentElement.clientHeight;
+    const bioDescY = bioDesc.getBoundingClientRect().y;
+    const bioDescHeight = bioDesc.getBoundingClientRect().height;
+
+    if (clientHeight > bioDescY + (bioDescHeight * 1) / 3) {
+      bioDesc.style.animation = "scroll 80s linear forwards";
+    } else {
+    }
+  });
   return (
-    <div className="Bio">
-      <h1>
-        <p>
-          Bio<br></br>
-          details about tech stack
-        </p>
-      </h1>
-    </div>
+    <body className="starWarsBody">
+      <div className="border">
+        <div className="desc">
+          <h1>Bio</h1>
+          <h2>Bio Desc</h2>
+          <p>
+            hello hello hello hello hello hello hello hello hello hello hello
+            hello hello hello hello hello hello hello hello hello hello hello
+            hello hello hello hello hello hello hello hello hello hello hello
+            hello hello hello hello hello hello hello hello hello hello hello
+            hello hello hello hello hello hello hello hello hello hello hello
+            hello hello hello hello hello hello hello hello hello hello hello
+            hello hello hello hello hello hello hello hello hello hello hello
+            hello hello hello hello hello hello hello hello hello hello hello
+            hello hello hello hello hello hello hello hello hello hello hello
+            hello hello hello hello hello hello hello hello hello hello hello
+            hello hello hello hello hello hello hello hello hello hello hello
+            hello hello hello hello hello hello hello hello hello hello hello
+            hello hello hello hello hello hello hello hello hello hello hello
+            hello hello hello hello hello hello hello
+          </p>
+        </div>
+      </div>
+    </body>
   );
 }
 
--- src/components/Video.js
+++ src/components/Video.js
@@ -4,14 +4,28 @@ import React from "react";
 
 import spaceVideo from "../assets/space.mp4";
 
-const Video = () => {
+function Video() {
+  // const videoRef = useRef();
+  // const setPlayBack = () => {
+  //   videoRef.current.playbackRate = 0.75;
+  // };
   return (
     <div className="movie">
-      <video autoPlay loop muted id="video">
+      <video
+        id="space"
+        autoPlay
+        loop
+        muted
+        // ref={videoRef}
+        // onCanPlay={() => setPlayBack()}
+      >
         <source src={spaceVideo} type="video/mp4" />
       </video>
+      <div className="content">
+        <h1>Brett Ruane</h1>
+      </div>
     </div>
   );
-};
+}
 
 export default Video;
