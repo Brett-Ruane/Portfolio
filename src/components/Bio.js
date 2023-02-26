4575e5228d53a2dc1553b7cbf874496304e24345
Brett-Ruane <bgr28288@uga.edu>
2023-02-26 12:33:45 -0500

    fixed scrolling

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,33 +1,7 @@
 import "./Bio.css";
+import "../functions/ScrollText.js";
 
 function Bio() {
-  // const viewportWidth = document.getElementById("viewport-width");
-  // const viewportHeight = document.getElementById("viewport-height");
-  // viewportWidth.innerHTML = document.documentElement.clientWidth;
-  // viewportHeight.innerHTML = document.documentElement.clientHeight;
-
-  // const topEdge = document.getElementById("top");
-  // const bottomEdge = document.getElementById("bottom");
-
-  const bioDesc = document.querySelector(".desc");
-  document.addEventListener("scroll", function (e) {
-    const clientHeight = document.documentElement.clientHeight;
-    const bioDescY = bioDesc.getBoundingClientRect().y;
-    // const bioDescHeight = bioDesc.getBoundingClientRect().height;
-
-    var moving = false;
-    if (clientHeight > bioDescY + 500 && moving === false) {
-      bioDesc.style.animation = "scroll 60s linear forwards";
-      moving = true;
-    } else {
-      bioDesc.style.animation = "";
-      moving = false;
-    }
-    if (clientHeight > bioDescY + 2500) {
-      bioDesc.style.animation = "";
-      moving = false;
-    }
-  });
   return (
     <body className="starWarsBody">
       <div className="border">
