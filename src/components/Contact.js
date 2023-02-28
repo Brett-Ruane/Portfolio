eed50ca3b575874816e8331a23d63744e41760b9
Brett-Ruane <bgr28288@uga.edu>
2023-02-28 01:55:11 -0500

    contact form works

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,55 +1,77 @@
-import { useState } from "react";
+import { useRef } from "react";
 import "./Contact.css";
+import emailjs from "@emailjs/browser";
 
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_gjasu0m",
+        "template_oz607x8",
+        form.current,
+        "5UyWYtolwvK4FVoj1"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <div className="Contact">
-      <form className="form">
-        <h1>Contact me</h1>
+      <form ref={form} onSubmit={sendEmail} className="form">
         <label>Name</label>
-        <NameField />
+        <input type="text" name="name" />
         <label>Email</label>
-        <EmailField />
+        <input type="email" name="email" />
         <label>Message</label>
-        <MessageField />
+        <textarea name="message" />
+        <input type="submit" value="Send" />
       </form>
     </div>
   );
 }
 
-function NameField() {
-  const [firstName, setFirstName] = useState("");
+// function NameField() {
+//   const [name, setName] = useState("");
 
-  return (
-    <div>
-      <input value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <input value={name} onChange={(e) => setName(e.target.value)} />
+//     </div>
+//   );
+// }
 
-function EmailField() {
-  const [email, setEmail] = useState("");
+// function EmailField() {
+//   const [email, setEmail] = useState("");
 
-  return (
-    <div>
-      <input value={email} onChange={(e) => setEmail(e.target.value)} />
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <input value={email} onChange={(e) => setEmail(e.target.value)} />
+//     </div>
+//   );
+// }
 
-function MessageField() {
-  const [message, setMessage] = useState("");
+// function MessageField() {
+//   const [message, setMessage] = useState("");
 
-  return (
-    <div>
-      <textarea
-        rows={"6"}
-        placeholder="Whatever you want to email me about"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <textarea
+//         rows={"6"}
+//         placeholder="Whatever you want to email me about"
+//         value={message}
+//         onChange={(e) => setMessage(e.target.value)}
+//       />
+//     </div>
+//   );
+// }
 
 export default Contact;
