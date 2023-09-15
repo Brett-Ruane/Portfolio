8dc6e7480c27da7b9a87016aced1c58713a8f617
Brett-Ruane <bgr28288@uga.edu>
2023-09-14 23:51:22 -0400

    commit to fix build fail

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+
+const pageTitle = "Brett Ruane";
+document.title = pageTitle;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -10,12 +12,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-const pageTitle = "Brett Ruane";
-document.title = pageTitle;
-
-document
-  // If you want to start measuring performance in your app, pass a function
-  // to log results (for example: reportWebVitals(console.log))
-  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-  .reportWebVitals();
