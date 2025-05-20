9c1bb854005ce972548beb3ec1fc3700071402d1
Brett Ruane <bgr28288@uga.edu>
2025-05-20 13:52:55 -0400

    adding Pad for spacing

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import TechStack from "./components/TechStack";
 import Bio from "./components/Bio";
 import Projects from "./components/Project";
 import HyperspaceTunnel from "./components/HyperspaceTunnel";
+import Pad from "./components/Pad";
 
 function App() {
   return (
     <>
       <HyperspaceTunnel />
       <Header />
+      <Pad />
       <Bio />
       <TechStack />
       <Projects />
---
+++ src/components/Pad.js
@@ -0,0 +1,18 @@
+import "./Bio.css";
+// import "../functions/ScrollText.js";
+
+function Pad() {
+  return (
+    <div className="starWarsBody">
+      <div className="border">
+        <div className="desc">
+          <h1></h1>
+          <h2></h2>
+          <p></p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Pad;
